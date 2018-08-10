2d180d66544e98fe6af93676164907e34d043cf8
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2018-08-10 23:58:58 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import { Panel } from "react-bootstrap";
+
+import './styles/styles.css'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col-sm-8 col-sm-offset-2">
+          <Panel>
+            <Panel.Body><p>Content goes here</p></Panel.Body>
+          </Panel>
         </div>
       </Provider>
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
 const logger = createLogger();
