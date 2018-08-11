6984d91ae897a9d16d7131d16a2ff630a09030b3
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2018-08-11 14:56:37 +0300

    Fix storing posts

--- src/redux/middleware/postsMdl.js
+++ src/redux/middleware/postsMdl.js
@@ -2,13 +2,10 @@ import * as types from '../actions/types';
 import {setPosts} from '../actions/postsActions';
 
 const storePosts = ({dispatch}) => next => action => {
-    if (action.type !== types.POSTS_FETCHING_SUCCEEDED) {
-        next(action);
-        return;
+    next(action);
+    if (action.type === types.POSTS_FETCHING_SUCCEEDED) {
+        dispatch(setPosts(action.payload));
     }
-
-    dispatch(setPosts(action.payload));
-
 };
 
 export default [storePosts];
